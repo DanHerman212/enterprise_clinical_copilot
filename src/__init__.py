cbe9d6d83d70518d309bb7c911f452fd28ee5d73
Dan Herman <dan.herman@me.com>
2026-05-24 12:33:03 -0400

    refreshed workspace to start over

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Reusable production code for the readmission pipeline.
-
-Modules:
-  config    -- project/region/table FQNs, MIMIC version, split contract.
-  tracking  -- Vertex AI Experiments wrapper (init, log_run, artifact upload).
-"""
